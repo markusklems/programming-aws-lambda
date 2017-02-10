54132a87b86c55c0d843e84d089f00a1921357e9
Markus Klems <markusklems@gmail.com>
2017-02-10 17:15:16 +0100

    Change handler.js code back to dummy code for purposes of consistency with video recording.

--- blog-app/handler.js
+++ blog-app/handler.js
@@ -1,20 +1,16 @@
 'use strict';
 
-module.exports.handler = (event, context, callback) => {
-const data = JSON.parse(event.body);
-if (data.text && typeof data.text !== 'string') {
-    console.error('Validation Failed');
-    callback(new Error('Body did not contain a text property.'));
-    return;
-}
-console.log(data.text);
-const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-        message: 'Created article.'
-    }),
-};
-'
-}),
-};
+module.exports.hello = (event, context, callback) => {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Go Serverless v1.0! Your function executed successfully!',
+            input: event,
+        }),
+    };
+
+    callback(null, response);
+
+    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+    // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
