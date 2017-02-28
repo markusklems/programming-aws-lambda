8d505cb349c759422504acaa21fec2213099b197
Markus Klems <markusklems@gmail.com>
2017-02-28 16:10:06 +0100

    Completed CRUD operations

--- pyblog-app/delete.py
+++ pyblog-app/delete.py
@@ -1,28 +1,25 @@
 from __future__ import print_function # Python 2/3 compatibility
 import json
 import boto3
-import uuid
 
 def handler(event, context):
-    print('received create event{}'.format(event))
+    print('received delete event{}'.format(event))
     dynamodb = boto3.resource('dynamodb', region_name='eu-central-1')
     table = dynamodb.Table('PyBlogTable')
-    id = str(uuid.uuid1())
     body = json.loads(event['body'])
-    text = body['text']
+    id = body['article_id']
 
-    put_response = table.put_item(
-        Item = {
-            'article_id': id,
-            'text': text
+    delete_response = table.delete_item(
+        Key = {
+            'article_id': id
             }
         )
 
-    print('put response{}'.format(put_response))
+    print('delete response{}'.format(delete_response))
 
     response = {
         "statusCode": 200,
-        "body": json.dumps({"article_id": id})
+        "body": json.dumps({"message":"deleted"})
     }
 
     return response
